e41f4d33a4a36fcd7a645ee02b4eabd1ea8cf0f4
Elysium Panda <54267065+Stella329@users.noreply.github.com>
2026-02-12 13:28:43 +0100

    dealt blank page on GitHub Pages by removing invalid `index.css` reference and conflicting `importmap`

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 注意：将其替换为您的 GitHub 仓库名称，例如 '/my-portfolio/'
+  // 注意：将其替换为您的 GitHub 仓库名称，例如 '/名称/'
   // 如果是部署到个人主页域名 (username.github.io)，则设为 根目录'/'
   base: '/My_Porfolio/', 
   build: {
